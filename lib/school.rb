282faba394601bc04275d3853e43d12f0194552b
Alec German <alec_german@yahoo.com>
2018-02-11 19:08:58 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  attr_reader :school, :roster
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student, grade)
+    @roster.include?(grade) ? @roster[grade] << student : @roster[grade] = [student]
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster[grade].values.sort{|a, b| a <=> b}
+  end
+end
